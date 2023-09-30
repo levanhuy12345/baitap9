12de907ee557ad54f44f0482975e33615142961c
levanhuy12345 <lehuy1234987@gmail.com>
2023-09-30 11:07:29 +0700

    baitap9

---
+++ View/Main.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package View;
+
+import Controller.ManagerMenu;
+
+/**
+ *
+ * @author PC
+ */
+public class Main {
+     public static void main(String[] args) {
+        ManagerMenu model = new ManagerMenu();
+        model.run();
+    }
+}
